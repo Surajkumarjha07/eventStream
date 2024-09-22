d3e17cb1f567781b3692c2a3ae15da60b9b1ad3f
Suraj kumar jha <surajkumarjha771@gmail.com>
2024-09-22 21:51:06 +0530

    The app is complete

--- frontend/src/components/navbar/navbar.component.ts
+++ frontend/src/components/navbar/navbar.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule, DOCUMENT, LowerCasePipe, UpperCasePipe } from '@angular/common';
-import { Component, Inject } from '@angular/core';
+import { Component, EventEmitter, Inject, OnInit, Output } from '@angular/core';
 import { RouterLink, RouterModule } from '@angular/router';
 
 @Component({
@@ -9,17 +9,46 @@ import { RouterLink, RouterModule } from '@angular/router';
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
   
   loginToken: any = ''
   email: any = ''
   name: any = ''
+  searchClicked!: boolean
+  @Output() searchActive = new EventEmitter<boolean>()
 
   constructor(@Inject(DOCUMENT) private document: Document) {
     const localStorage = document.defaultView?.localStorage;
     this.loginToken = localStorage?.getItem('loginToken')
     this.email = localStorage?.getItem('userEmail')
     this.name = localStorage?.getItem('userName')
+  }
+
+  ngOnInit(): void {
+      this.searchInActive()
+  }
+
+  search(e: Event) {
+    let target = e.target as HTMLInputElement
+    // if (target.) {
+    // }
+  }
+
+  toggleSuggestion() {
+    this.searchClicked = true
+    this.searchActive.emit(true)
+  }
+
+
+  searchInActive() {
+    this.document.addEventListener('click', (event) => {
+      let target = event.target as HTMLElement
+      
+      if (!target.classList.contains('search-container')) {
+        this.searchClicked = false
+        this.searchActive.emit(false)
+      }
+    })
   }
 
 }
